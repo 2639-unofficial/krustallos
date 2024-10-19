d6318938bf89a9e9c39851b3beeb0b1a5a276c99
2639 <2639nydna6462@gmail.com>
2024-10-20 00:02:25 +0800

    Remove `system.copySystemConfiguration` option

--- hosts/mfm8s/configuration.nix
+++ hosts/mfm8s/configuration.nix
@@ -103,7 +103,8 @@
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
+  # NOTE: Not compatible with flakes.
+  # system.copySystemConfiguration = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
