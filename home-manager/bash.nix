1d9f14977f8523463e547af79fd88a7a42737d3b
2639 <2639nydna6462@gmail.com>
2025-06-27 19:51:44 +0800

    bash: Update config

--- home-manager/bash.nix
+++ home-manager/bash.nix
@@ -1,45 +1,64 @@
-{ ... }:
-
+{ options, ... }:
+let
+  shellOptionsDefault = options.programs.bash.shellOptions.default;
+in
 {
   programs.bash = {
     enable = true;
 
     shellAliases = {
-      # Basic
+      # Basics
       l = "ls -Alh --color=auto";
+      "c-" = "cd -";
       ".." = "cd ..";
       "..." = "cd ../..";
 
       # Fast scroll back buffer clearing, like `clear`
       rst = "tput reset";
+
+      # Check $PATH
+      # path = "echo $PATH | tr ':' '\n'";
+      # path = "echo $PATH | sed 's/:/\n/g'";
+      path = "echo $PATH | awk --assign=RS=':' '{print}'";
     };
 
-    initExtra = ''
+    initExtra = /* bash */ ''
       # Create every dir along the path and cd to the path
       mkcd() {
         mkdir -p "$@" && cd "$_"
       }
 
-      # cd up a number of directories
-      up() {
-        local d=""
-        local limit="$1"
-
-        # Default to limit of 1
-        if [ -z "$limit" ] || [ "$limit" -le 0 ]; then
-          limit=1
-        fi
-
-        for ((i=1;i<=limit;i++)); do
-          d="../$d"
-        done
-
-        # perform cd. Show error if cd fails
-        if ! cd "$d"; then
-          echo "Can't move up $limit directories.";
-        fi
+      # Return to project root
+      rpr() {
+        local root
+        root=$(git rev-parse --show-toplevel 2>/dev/null) || {
+            echo "Error: Not inside a Git repository." >&2
+            return 1
+        }
+        cd "$root"
       }
+
+      # Show directory contents when changing to a new directory,
+      # with a size threshold to avoid performance hit
+      export PROMPT_COMMAND='
+        if [[ -n "$__new_workdir" && "$PWD" != "$__new_workdir" ]]; then
+          file_count=$(find "$PWD" -maxdepth 1 -type f | wc -l)
+          if [[ $file_count -lt 1000 ]]; then
+            l
+          fi
+        fi
+        __new_workdir=$PWD
+        '
     '';
+
+    shellOptions = shellOptionsDefault ++ [ "autocd" ];
+  };
+
+  programs.readline = {
+    enable = true;
+    includeSystemConfig = true;
+    variables = {
+      completion-ignore-case = true;
+    };
   };
 }
-
