fa6514d256b0252e629ecaa5da5240341cb1141d
2639 <2639nydna6462@gmail.com>
2025-06-27 20:09:43 +0800

    nix-ld: Update config

--- hosts/common/nix-ld.nix
+++ hosts/common/nix-ld.nix
@@ -4,6 +4,11 @@
   # INFO: https://wiki.nixos.org/wiki/FAQ#I've_downloaded_a_binary%2C_but_I_can't_run_it%2C_what_can_I_do%3F
   programs.nix-ld = {
     enable = true;
-    libraries = pkgs.steam-run.fhsenv.args.multiPkgs pkgs;
+    libraries = pkgs.steam-run.args.multiPkgs pkgs;
   };
+
+  # https://fzakaria.com/2025/02/26/nix-pragmatism-nix-ld-and-envfs.html
+  # services.envfs = {
+  #   enable = true;
+  # };
 }
