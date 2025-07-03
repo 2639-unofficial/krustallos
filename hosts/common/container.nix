eeb6ecd9ab09e8596ec914c54c36186ae6829424
2639 <2639nydna6462@gmail.com>
2025-07-03 22:34:42 +0800

    waydroid: Add notes

--- hosts/common/container.nix
+++ hosts/common/container.nix
@@ -10,4 +10,13 @@
     # Required for containers under podman-compose to be able to talk to each other
     defaultNetwork.settings.dns_enabled = true;
   };
+
+  # NOTE: See https://wiki.nixos.org/wiki/Waydroid, https://docs.waydro.id/,
+  #       and https://wiki.archlinux.org/title/Waydroid for usage
+  # NOTE: Usable but the UX isn't great (e.g. zooming in google map requires
+  #       double clicking the left mouse btn then dragging the mouse up or down;
+  #       related: https://github.com/waydroid/waydroid/issues/1087)
+  # NOTE: Also, `waydroid prop set persist.waydroid.fake_touch "*"` does not
+  #       work in GraspJPN, a Japanese vocabulary app
+  # virtualisation.waydroid.enable = true;
 }
