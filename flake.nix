75b51c7966c296e8a60335bce9ebcbfbf4ab4926
2639 <2639nydna6462@gmail.com>
2025-07-22 08:44:50 +0800

    flake: Comment out nixConfig by default
    Shouldn't have set it in the first place, due to the security
    implications. Leaving it as a note for future reference.
    
    See also https://docs.lix.systems/manual/lix/stable/installation/multi-user.html#trusted-users

--- flake.nix
+++ flake.nix
@@ -54,27 +54,27 @@
   };
 
   # INFO: Nix stores the options to $HOME/.local/share/nix/trusted-settings.json
-  # when user agrees to "permenently trust" the settings
-  nixConfig = {
-    # NOTE: These options do not work for two reasons:
-    # 1) Without `accept-flake-config` being enabled, only a small portion of
-    #    options are allowed here. See section nix3-flake#flake-format in the
-    #    nix manual for more details.
-    # 2) Without the user being added to `trusted-users`, the `trusted-public-keys`
-    #    option will be ignored, even if the user is added to `allowed-users` and
-    #    the `trusted-substituters` option is properly set.
-    #    See https://github.com/NixOS/nix/issues/1921
-    #    Related: https://github.com/nixos/nix/issues/8973
-    #    However, being added to `trusted-users` poses a critical security risk,
-    #    per https://github.com/NixOS/nix/issues/9649#issuecomment-1868001568:
-    #    > With your user in trusted-users:
-    #    > ```bash
-    #    > sudo2() {
-    #    >   NIX_CONFIG="post-build-hook = $@" nix build nixpkgs#hello --rebuild --print-build-logs;
-    #    > }
-    #    > ```
-    #    > Brand new sudo, now without password!
-    extra-trusted-substituters = [ "https://helix.cachix.org" ];
-    extra-trusted-public-keys = [ "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs=" ];
-  };
+  # when user agrees to "permanently trust" the settings
+  # nixConfig = {
+  #   # NOTE: These options do not work for two reasons:
+  #   # 1) Without `accept-flake-config` being enabled, only a small portion of
+  #   #    options are allowed here. See section nix3-flake#flake-format in the
+  #   #    nix manual for more details.
+  #   # 2) Without the user being added to `trusted-users`, the `trusted-public-keys`
+  #   #    option will be ignored, even if the user is added to `allowed-users` and
+  #   #    the `trusted-substituters` option is properly set.
+  #   #    See https://github.com/NixOS/nix/issues/1921
+  #   #    Related: https://github.com/nixos/nix/issues/8973
+  #   #    However, being added to `trusted-users` poses a critical security risk,
+  #   #    per https://github.com/NixOS/nix/issues/9649#issuecomment-1868001568:
+  #   #    > With your user in trusted-users:
+  #   #    > ```bash
+  #   #    > sudo2() {
+  #   #    >   NIX_CONFIG="post-build-hook = $@" nix build nixpkgs#hello --rebuild --print-build-logs;
+  #   #    > }
+  #   #    > ```
+  #   #    > Brand new sudo, now without password!
+  #   extra-trusted-substituters = [ "https://helix.cachix.org" ];
+  #   extra-trusted-public-keys = [ "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs=" ];
+  # };
 }
