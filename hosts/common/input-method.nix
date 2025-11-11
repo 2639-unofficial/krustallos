165d5549af78f8fb669456248ca94bca9b58be83
2639 <2639nydna6462@gmail.com>
2025-11-11 14:28:56 +0800

    fcitx5: Add input methods and themes
    Great UX with mcbopomofo

--- hosts/common/input-method.nix
+++ hosts/common/input-method.nix
@@ -7,7 +7,12 @@
     type = "fcitx5";
     fcitx5 = {
       waylandFrontend = true;
-      addons = [ pkgs.fcitx5-chewing ];
+      addons = with pkgs; [
+        fcitx5-chewing
+        fcitx5-mcbopomofo
+        fcitx5-mellow-themes
+        fcitx5-mozc-ut
+      ];
     };
   };
 }
