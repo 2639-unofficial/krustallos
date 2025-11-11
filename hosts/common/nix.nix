5e4baf20ba581169dedf7f2d5babc225336e1221
2639 <2639nydna6462@gmail.com>
2025-11-11 14:28:56 +0800

    nix: Enable pipe-operators
    For experimenting with other people's package that depends on it

--- hosts/common/nix.nix
+++ hosts/common/nix.nix
@@ -12,7 +12,7 @@
 
     settings = {
       # Enable flakes and new `nix` command
-      experimental-features = [ "nix-command" "flakes" ];
+      experimental-features = [ "nix-command" "flakes" "pipe-operators" ];
       # Deduplicate and optimize nix store
       # PERF: Could cause I/O overhead; consider running `nix store optimise`
       # manually, or setting `nix.optimise` instead
