63cd68de527892ce82f37f027c563ec1d1f6ce0a
2639 <2639nydna6462@gmail.com>
2026-05-05 02:39:51 +0800

    minor: Utilize `krustallos-path`

--- home-manager/wlr-which-key.nix
+++ home-manager/wlr-which-key.nix
@@ -1,6 +1,6 @@
-{ config, pkgs, ... }:
+{ config, pkgs, krustallos-path, ... }:
 let
-  config-path = "${config.home.homeDirectory}/krustallos/home-manager/wlr-which-key";
+  config-path = "${krustallos-path}/home-manager/wlr-which-key";
 in
 {
   # Workaround before niri supports binding submaps
