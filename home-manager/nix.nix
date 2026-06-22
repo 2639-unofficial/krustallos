830641c7c39c1a20f314116526674bb02af978fd
2639 <2639nydna6462@gmail.com>
2026-06-23 05:33:34 +0800

    flake: More aliases for quick edit

--- home-manager/nix.nix
+++ home-manager/nix.nix
@@ -24,9 +24,11 @@
     flake = krustallos.path;
   };
 
-  # Aliases for navigating to the flake, utilizing the env var set by nh
+  # Aliases for accessing the flake, utilizing the env var set by nh
   programs.nushell.shellAliases."cdf" = "cd $env.NH_FLAKE";
+  programs.nushell.shellAliases."hxf" = "hx $env.NH_FLAKE";
   programs.bash.shellAliases."cdf" = "cd $NH_FLAKE";
+  programs.bash.shellAliases."hxf" = "hx $NH_FLAKE";
 
   # `nix run n#hello` with ease!
   # NOTE: Setting an entry in the user registry, instead of the system registry,
