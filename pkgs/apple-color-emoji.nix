d26217baaba30d1ce817b94e7b4edf192d950bd5
2639 <2639nydna6462@gmail.com>
2026-08-30 17:56:35 +0800

    apple-color-emoji: Update

--- pkgs/apple-color-emoji.nix
+++ pkgs/apple-color-emoji.nix
@@ -8,11 +8,11 @@ stdenvNoCC.mkDerivation (finalAttrs: {
   # NOTE: See https://github.com/nixos/nixpkgs/issues/261100 for why it's not included in nixpkgs
   # NOTE: Not using the project's flake due to its long build time
   pname = "apple-color-emoji";
-  version = "18.4";
+  version = "26-20260722-484daf4e";
 
   src = fetchurl {
-    url = "https://github.com/samuelngs/apple-emoji-linux/releases/download/v${finalAttrs.version}/AppleColorEmoji.ttf";
-    hash = "sha256-pP0He9EUN7SUDYzwj0CE4e39SuNZ+SVz7FdmUviF6r0=";
+    url = "https://github.com/samuelngs/apple-emoji-ttf/releases/download/macos-${finalAttrs.version}/AppleColorEmoji-linux.ttf";
+    hash = "sha256-43x69iZaxKCvbVe8ZehhCad22ZZug0MzRVf2PaSCUW8=";
   };
 
   # Skip the unpack phase
@@ -21,11 +21,14 @@ stdenvNoCC.mkDerivation (finalAttrs: {
   # NOTE: With EXT4, the ttf file (43 MB) is duplicated, thereby doubling the closure size.
   # Should we `ln -s` instead?
   installPhase = ''
+    runHook preInstall
     install -D -m 644 -t "$out/share/fonts/truetype/" $src
+    runHook postInstall
   '';
 
   meta = {
     description = "A derivation for the Apple Color Emoji font";
-    platform = lib.platforms.all;
+    homepage = "https://github.com/samuelngs/apple-emoji-ttf";
+    platform = lib.platforms.linux;
   };
 })
