e90ea9019ee8fadc65cf0b89f4f5ef1b698eeb0e
2639 <2639nydna6462@gmail.com>
2026-09-07 05:30:41 +0800

    minor: Refine overlay format

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,16 +5,13 @@
 
   modifications = final: prev: {
     # Update jj-fzf
-    jj-fzf = (
-      prev.lib.warnIf (prev.lib.versionOlder "0.38.0" prev.jj-fzf.version)
-        "jj-fzf > 0.38.0 is now in nixpkgs, the override can be removed."
-        (
-          prev.jj-fzf.overrideAttrs {
-            version = "0.42.0";
-            src = inputs.jj-fzf;
-            patches = [ ./jj-fzf-preflight.patch ];
-          }
-        )
-    );
+    jj-fzf = prev.jj-fzf.overrideAttrs {
+      version = "0.42.0";
+      src =
+        prev.lib.warnIf (prev.lib.versionOlder "0.38.0" prev.jj-fzf.version)
+          "jj-fzf > 0.38.0 is updated in upstream"
+          inputs.jj-fzf;
+      patches = [ ./jj-fzf-preflight.patch ];
+    };
   };
 }
